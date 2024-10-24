189afff5dd4de1e2ac1cc9cf5bd32c65b48adeb8
Niffy-rocks2 <nifemioredola324@gmail.com>
2024-10-24 03:12:06 +0100

    ('practice)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,13 @@
+fn main() {
+   
+    println!();
+    
+    
+    println!("helloooo");
+    
+    
+    println!("format {} arguments", "some");
+    
+    
+    println!("My name is {}. I love computer science", "Dagogo William-jumbo");
+}

ec2212881bcf33e2350abf6bfa8838d585772df0
Niffy-rocks2 <nifemioredola324@gmail.com>
2024-10-26 19:46:08 +0100

    (project_3correct)

--- week-2/project_3correct.rs
+++ week-2/project_3correct.rs
@@ -4,7 +4,7 @@ let r:f64 = 5.0;
 let t:f64 = 3.0;
 
  // A
- let a = p * (1.0-(r / 100.0 )) * t;
+ let a = p * (1.0-(r / 100.0 )).powi(t as i32);
  println!("Amount is {}", a);
  let ci = a - p;
  println!("compound interest is {}", ci);
